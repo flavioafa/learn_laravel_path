4f4656634f68f921ed827902501251f3a3a4b9b8
Flávio Araújo <flavioafa@gmail.com>
2023-06-23 20:07:51 -0300

    The Laravel Installer Tool - Seção 2 - Use the Filesystem Class to Read a Directory - Obtendo os arquivos dos posts dinamicamente; Usando a classe File.

--- 05_laravel_8_from_scratch/blog/resources/views/posts.blade.php
+++ 05_laravel_8_from_scratch/blog/resources/views/posts.blade.php
@@ -4,37 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First post</a></h1>
-        <p>
-            It is a long established fact that a reader will be distracted by the readable content of a page when
-            looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-            letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-            desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-            search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved
-            over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second post</a></h1>
-        <p>
-            It is a long established fact that a reader will be distracted by the readable content of a page when
-            looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-            letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-            desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-            search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved
-            over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My Third post</a></h1>
-        <p>
-            It is a long established fact that a reader will be distracted by the readable content of a page when
-            looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-            letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-            desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-            search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved
-            over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
--- 05_laravel_8_from_scratch/blog/routes/web.php
+++ 05_laravel_8_from_scratch/blog/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts');
+Route::get('/', function ()
+{
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
-Route::get('/posts/{post}', function ($slug) {
-    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-    return view('post', ['post' => $post]);
+Route::get('/posts/{post}', function ($slug)
+{
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
 })->where('post', '[A-z_\-]+');
