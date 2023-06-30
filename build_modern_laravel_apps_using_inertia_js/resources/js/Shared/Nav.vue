5727b76b9fd8a45641884ecf30d0f3b84be0c17c
Flávio Araújo <flavioafa@gmail.com>
2023-06-30 11:05:06 -0300

    Build Modern Laravel Apps Using Inertia.js - Seção 1 - Perform Non-GET Requests - Adicionado tailwindcss; Exemplo de POST, evitando a possibilidade de ctrl + click;

--- build_modern_laravel_apps_using_inertia_js/resources/js/Shared/Nav.vue
+++ build_modern_laravel_apps_using_inertia_js/resources/js/Shared/Nav.vue
@@ -1,14 +1,17 @@
 <template>
-    <nav>
-        <ul>
+    <nav class="mt-6">
+        <ul class="list-disc">
             <li>
-                <Link href="/">Home</Link>
+                <Link class="text-blue-500 hover:underline" href="/">Home</Link>
             </li>
             <li>
-                <Link href="/users">Users</Link>
+                <Link class="text-blue-500 hover:underline" href="/users">Users</Link>
             </li>
             <li>
-                <Link href="/settings">Settings</Link>
+                <Link class="text-blue-500 hover:underline" href="/settings">Settings</Link>
+            </li>
+            <li>
+                <Link class="text-blue-500 hover:underline" href="/logout" method="post" as="button">Log Out</Link>
             </li>
         </ul>
     </nav>
