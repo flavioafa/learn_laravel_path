58072893bb432f46d97bc3bc7257021873180455
Flávio Araújo <flavioafa@gmail.com>
2023-06-30 14:41:23 -0300

    Build Modern Laravel Apps Using Inertia.js - Seção 1 - An Important SPA Security Concern - Retornando dados para a página; Apenas os campos necessários;

--- build_modern_laravel_apps_using_inertia_js/resources/js/Pages/Users.vue
+++ build_modern_laravel_apps_using_inertia_js/resources/js/Pages/Users.vue
@@ -3,4 +3,13 @@
     <h1 class="text-4xl font-bold">
         Users
     </h1>
+    <ul>
+        <li v-for="user in users" :key="user.id" v-text="user.name"></li>
+    </ul>
 </template>
+
+<script setup>
+defineProps({
+    users: Array
+})
+</script>
