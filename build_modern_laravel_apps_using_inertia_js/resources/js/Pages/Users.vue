c1f2e3e6e415fb8f4bc736a5fe3d1fa385819c53
Flávio Araújo <flavioafa@gmail.com>
2023-06-30 15:36:47 -0300

    Build Modern Laravel Apps Using Inertia.js - Seção 1 - Pagination - Adicionado o componente paginator; Adicionado a lista de usuários no front;

--- build_modern_laravel_apps_using_inertia_js/resources/js/Pages/Users.vue
+++ build_modern_laravel_apps_using_inertia_js/resources/js/Pages/Users.vue
@@ -3,13 +3,38 @@
     <h1 class="text-4xl font-bold">
         Users
     </h1>
-    <ul>
-        <li v-for="user in users" :key="user.id" v-text="user.name"></li>
-    </ul>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full text-left text-sm font-light">
+                        <thead class="border-b font-medium dark:border-neutral-500">
+                            <tr>
+                                <th scope="col" class="px-6 py-4">#</th>
+                                <th scope="col" class="px-6 py-4">Name</th>
+                                <th scope="col" class="px-6 py-4">Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="user in users.data" :key="user.id" class="border-b dark:border-neutral-500">
+                                <td class="whitespace-nowrap px-6 py-4 font-medium">{{ user.id }}</td>
+                                <td class="whitespace-nowrap px-6 py-4">{{ user.name }}</td>
+                                <td class="whitespace-nowrap px-6 py-4">
+                                    <Link :href="`/users/${user.id}/edit`">Edit</Link>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <Pagination :links="users.links" class="mt-6" />
 </template>
 
 <script setup>
+import Pagination from '../Shared/Pagination.vue';
 defineProps({
-    users: Array
+    users: Object
 })
 </script>
---
+++ build_modern_laravel_apps_using_inertia_js/resources/js/Shared/Pagination.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- Paginator -->
+    <div>
+        <component v-for="link in links" :href="link.url" v-html="link.label" :is="link.url ? 'Link' : 'span'" class="px-1"
+            :class="{ 'text-gray-500': ! link.url, 'font-bold': link.active }">
+        </component>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    links: Array
+})
+</script>
