efa68b1f013ac8c072ad5281e9ff947caa5674ea
Flávio Araújo <flavioafa@gmail.com>
2023-06-30 10:17:48 -0300

    Build Modern Laravel Apps Using Inertia.js - Seção 1 - Install and Configure Inertia - Adicionada outra forma de entregar as páginas; Adicionado import de css no app.blade.php;

--- build_modern_laravel_apps_using_inertia_js/resources/js/app.js
+++ build_modern_laravel_apps_using_inertia_js/resources/js/app.js
@@ -1,11 +1,23 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+//Link interessante
+// https://github.com/laravel/vite-plugin/blob/main/UPGRADE.md#migrating-from-vite-to-laravel-mix
+//Qual a diferença entre essas duas forma de resolve de página?
+    //O primeiro veio da documentação do inertia, o segundo do link acima
+    //Percebi que o segundo, na build, gera vários arquivos
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
-  },
+//   resolve: name => {
+//     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+//     return pages[`./Pages/${name}.vue`]
+//   },
+  resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
