4beb0ed99ac946af2e6aaf28bf8e8068f9106561
Flávio Araújo <flavioafa@gmail.com>
2023-06-30 11:13:47 -0300

    Build Modern Laravel Apps Using Inertia.js - Seção 1 - Preserve the Scroll Position - Adicionado exemplo de preserve-scroll;

--- build_modern_laravel_apps_using_inertia_js/resources/js/Pages/Users.vue
+++ build_modern_laravel_apps_using_inertia_js/resources/js/Pages/Users.vue
@@ -1,10 +1,20 @@
 <template>
-    <h1>
+    <h1 class="text-4xl font-bold">
         Users
     </h1>
     <Nav />
+    <div style="margin-top:1400px">
+        The current time is {{ time }}
+    </div>
+    <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
 </template>
 
 <script setup>
 import Nav from '../Shared/Nav.vue'
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    time: String
+})
+
 </script>
