b2318f1314141aaf70b96737f91688ff459917bc
Flávio Araújo <flavioafa@gmail.com>
2023-07-01 18:12:02 -0300

    Inertia and SPA Techniques - Seção 1 - Import Aliases - Adicionadas configurações para possibilitar o import através de alias no vite; Adicionado configuração do prettier para formatar os arquivos no padrão do projeto;

--- inertia_and_spa_techniques/vite.config.js
+++ inertia_and_spa_techniques/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
-import vue from "@vitejs/plugin-vue";
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
 import DefineOptions from 'unplugin-vue-define-options/vite'
+import path from 'path'
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve('resources/js')
+        }
+    },
     plugins: [
         vue(),
         DefineOptions(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+            refresh: true
+        })
+    ]
+})
