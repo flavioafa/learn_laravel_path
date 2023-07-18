268c607ba106cc29a8490785c822617456583594
Flávio Araújo <flavioafa@gmail.com>
2023-07-18 20:15:25 -0300

    Laravel 8 From Scratch - Seção 4 - Make a Post Model and Migration - Implementando a utilização de Model eloquent em Post;

--- 05_laravel_8_from_scratch/resources/views/posts.blade.php
+++ 05_laravel_8_from_scratch/resources/views/posts.blade.php
@@ -1,12 +1,12 @@
 <x-layout>
     @foreach ($posts as $post)
-        <article>
-            <a href="/posts/{{ $post->slug }}">
-                <h1>{{ $post->title }}</h1>
-            </a>
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
+    <article>
+        <a href="/posts/{{ $post->id }}">
+            <h1>{{ $post->title }}</h1>
+        </a>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
     @endforeach
 </x-layout>
